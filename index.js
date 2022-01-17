2e29445d03b77b0da2becc1599e4cdde35db93f5
Iuliia El Sammak <juliag875@gmail.com>
2022-01-16 23:31:36 -0500

    commit tree c8ee195cf9c20ce81a982c53952c93e82f6fbeea

--- index.js
+++ index.js
@@ -2,29 +2,29 @@
 const dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 
-
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
 
-    if ( left > 0 ) {
+    if (left > 0) {
         dodger.style.left = `${left-1}px`;
     }
 }
 
 function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
+    const left = parseInt(leftNumbers,10);
 
-    if ( left < 360 ) {
-        dodger.style.left= `${left+1}px`;
+    if (left < 360) {
+        dodger.style.left = `${left+1}px`;
     }
 }
 
 document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
         moveDodgerLeft();
-    } else if (e.key === "ArrowRight")  {
+    } else if (e.key === "ArrowRight") {
         moveDodgerRight();
     }
 });
+
